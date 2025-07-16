8897ce2cf1ef381e96db0ffd2686bca2ce424b3a
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2025-07-16 13:04:33 +0000

    removed duplicate images, pruned image pool

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="Image Tagging Interface", layout="wide")
 
 pg = st.navigation([
     st.Page("home.py",title="Home"),
-    #st.Page("image_tagger.py",title="Image Tagger",icon='💎'),
+    # st.Page("image_tagger.py",title="Image Tagger",icon='💎'),
     st.Page("ab_tester.py", title="Hot or Not?", icon = '🔥')
 ])
 pg.run()
